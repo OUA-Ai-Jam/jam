73e4845df4b8ecb6b8cdd279b78859e512ffac47
Batuhan Aksüt <batuhanaksut@hotmail.com>
2024-05-21 21:37:24 +0300

    debug banner kaldırıldı

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Paralel Evren Hikaye Uygulaması',
       theme: ThemeData(
         primarySwatch: Colors.green,
